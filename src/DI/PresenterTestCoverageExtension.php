a370178c5ff5cefc070e2941369bb266e8cd6233
Tomáš Kudělka <thorewi@gmail.com>
2022-01-08 10:48:32 +0100

    Update PresenterTestCoverageExtension.php

--- src/DI/PresenterTestCoverageExtension.php
+++ src/DI/PresenterTestCoverageExtension.php
@@ -5,26 +5,28 @@ namespace ADT\PresenterTestCoverage\DI;
 use ADT\PresenterTestCoverage\Console\CheckUrlCommand;
 use ADT\PresenterTestCoverage\Service;
 
-class PresenterTestCoverageExtension extends \Nette\DI\CompilerExtension {
-
-	public function loadConfiguration() {
-		$builder = $this->getContainerBuilder();
+class PresenterTestCoverageExtension extends \Nette\DI\CompilerExtension 
+{
+	public function loadConfiguration(): void
+	{
 		$config = $this->validateConfig([
-			"appNamespacePrefix" => "App",
-			"crawlerNamespacePrefix" => "Url",
-			"testDir" => NULL,
-			"tempDir" => NULL,
+			'appNamespacePrefix' => 'App',
+			'crawlerNamespacePrefix' => NULL,
+			'presenterDir' => NULL,
+			'tempDir' => NULL,
+			'testDir' => NULL,
 		]);
+
+		$builder = $this->getContainerBuilder();
 
 		// command pro kontrolu URL tříd
 		$builder->addDefinition($this->prefix('command'))
 			->setClass(CheckUrlCommand::class)
-			->addSetup("setConfig", [$config])
+			->addSetup('setConfig', [$config])
 			->addTag('kdyby.console.command');
 
 		$builder->addDefinition($this->prefix('service'))
 			->setClass(Service::class)
-			->addSetup("setConfig", [$config]);
+			->addSetup('setConfig', [$config]);
 	}
-
 }
